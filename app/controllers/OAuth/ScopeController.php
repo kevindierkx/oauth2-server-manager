3e50597a44c789f902b5a99abad8090b6a038112
Niels Tholenaar <niels@pcextreme.nl>
2015-04-01 22:27:00 +0200

    Added possibility to create scopes

--- app/controllers/OAuth/ScopeController.php
+++ app/controllers/OAuth/ScopeController.php
@@ -1,10 +1,28 @@
 <?php namespace OAuth;
 
 use BaseController;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\Validation\Factory as ValidationFactory;
 use Illuminate\View\Factory as ViewFactory;
 use OAuth\ScopeRepositoryInterface;
 
 class ScopeController extends BaseController {
+
+	/**
+	 * @var \Illuminate\Http\Request
+	 */
+	protected $request;
+
+	/**
+	 * @var \Illuminate\Routing\Redirector
+	 */
+	protected $redirector;
+
+	/**
+	 * @var \Illuminate\Validation\Factory
+	 */
+	protected $validationFactory;
 
 	/**
 	 * @var \OAuth\ScopeRepositoryInterface
@@ -12,17 +30,39 @@ class ScopeController extends BaseController {
 	protected $scopes;
 
 	/**
+	 * @var array
+	 */
+	protected $rules = [
+		'create' => [
+			'id'          => 'required|unique:scopes',
+			'description' => 'required',
+		],
+	];
+
+	/**
 	 * Bind required instances to the class.
 	 *
-	 * @param  \Illuminate\View\Factory         $viewFactory
-	 * @param  \OAuth\ScopeRepositoryInterface  $scopes
+	 * @param  \Illuminate\Http\Request          $request
+	 * @param  \Illuminate\Routing\Redirector    $redirector
+	 * @param  \Illuminate\Validation\Factory    $validationFactory
+	 * @param  \Illuminate\View\Factory          $viewFactory
+	 * @param  \OAuth\ScopeRepositoryInterface   $scopes
 	 */
 	public function __construct(
+		Request $request,
+		Redirector $redirector,
+		ValidationFactory $validationFactory,
 		ViewFactory $viewFactory,
 		ScopeRepositoryInterface $scopes
 	)
 	{
 		parent::__construct($viewFactory);
+
+		$this->request = $request;
+
+		$this->redirector = $redirector;
+
+		$this->validationFactory = $validationFactory;
 
 		$this->scopes = $scopes;
 	}
@@ -42,6 +82,46 @@ class ScopeController extends BaseController {
 
 		return $this->layout
 			->with('content', $content);
+	}
+
+	/**
+	 * Display create scope form.
+	 *
+	 * @return \Illuminate\View\View
+	 */
+	public function create()
+	{
+		$content = $this->viewFactory->make('scopes.create');
+
+		return $this->layout
+			->with('content', $content);
+	}
+
+	/**
+	 * Store the scope
+	 *
+	 * @return \Illuminate\Http\RedirectResponse
+	 */
+	public function store()
+	{
+		$validator = $this->validationFactory->make(
+			$this->request->except('_token', 'submit'),
+			$this->rules['create']
+		);
+
+		if ( $validator->fails() ) {
+			return $this->redirector->back()
+				->withInput()
+				->withErrors($validator);
+		}
+
+		$this->scopes->create(
+			$this->request->get('id'),
+			$this->request->get('description')
+		);
+
+		return $this->redirector->route('oauth.scopes.index')
+			->with('success', "Scope added successfully.");
 	}
 
 }
---
+++ app/views/scopes/create.blade.php
@@ -0,0 +1,18 @@
+{{ BootForm::open()->action(route('oauth.scopes.store')) }}
+{{ BootForm::token() }}
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h2 class="panel-title">
+				Add Scope
+			</h2>
+		</div>
+
+		<div class="panel-body">
+			@include('scopes.partials_create.form')
+		</div>
+
+		<div class="panel-footer">
+			{{ BootForm::submit('Add scope', 'btn-primary') }}
+		</div>
+	</div>
+{{ BootForm::close() }}
